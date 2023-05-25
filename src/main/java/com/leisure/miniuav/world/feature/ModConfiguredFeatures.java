8932f9c5ddf6884860b7f84c8d15b16af6d3dbaf
leisure <491653240@qq.com>
2023-05-25 20:46:30 +0800

    新增矿石生成逻辑实例，未添加材质文件

--- src/main/java/com/leisure/miniuav/world/feature/ModConfiguredFeatures.java
+++ src/main/java/com/leisure/miniuav/world/feature/ModConfiguredFeatures.java
@@ -4,32 +4,74 @@ import com.google.common.base.Suppliers;
 import com.leisure.miniuav.block.BlockRegister;
 import com.leisure.miniuav.utils.Reference;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.data.worldgen.features.OreFeatures;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.*;
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
+import java.util.ResourceBundle;
 import java.util.function.Supplier;
 
 
 public class ModConfiguredFeatures {
-//    public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURES =
-//            DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, Reference.MOD_ID);
-//
-//    //将我们第一步的两种矿石分别填入其中
-//    public static final Supplier<List<OreConfiguration.TargetBlockState>> OVERWORLD_FANTOM_ORES = Suppliers.memoize(() -> List.of(
-//            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegister.FANTOM_ORE.get().defaultBlockState()), //普通岩层
-//            OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockRegister.DEEPSLATE_FANTOM_ORE.get().defaultBlockState()))); //深板岩层
-//
-//    //将这种矿石生成类型进行注册
-//    public static final RegistryObject<ConfiguredFeature<?, ?>> FANTOM_ORES = CONFIGURED_FEATURES.register("fantom_ore",
-//            () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_FANTOM_ORES.get(),7)));
-//
-//    public static void register(IEventBus eventBus) {
-//        CONFIGURED_FEATURES.register(eventBus);
-//    }
+
+    public static final ResourceKey<ConfiguredFeature<?,?>> EBONY_KEY = registerKey("ebony");
+
+    public static final ResourceKey<ConfiguredFeature<?,?>> OVERWORLD_BLACK_OPAL_ORE_KEY = registerKey("black_opal_ore");
+    public static final ResourceKey<ConfiguredFeature<?,?>> END_BLACK_OPAL_ORE_KEY = registerKey("end_black_opal_ore");
+    public static final ResourceKey<ConfiguredFeature<?,?>> NETHER_BLACK_OPEL_ORE_KEY = registerKey("nether_black_opel_ore");
+
+
+    public static void bootstrap(BootstapContext<ConfiguredFeature<?,?>> context){
+        RuleTest stoneReplaceAbles = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepSlateReplaceAbles = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+        RuleTest netherRackReplaceAbles = new BlockMatchTest(Blocks.NETHERRACK);
+        RuleTest endStoneReplaceAbles = new BlockMatchTest(Blocks.END_STONE);
+
+        List<OreConfiguration.TargetBlockState> overWorldBlackOpalOres = List.of(OreConfiguration.target(stoneReplaceAbles,
+                BlockRegister.FANTOM_ORE.get().defaultBlockState()),
+                OreConfiguration.target(deepSlateReplaceAbles, BlockRegister.DEEPSLATE_FANTOM_ORE.get().defaultBlockState()));
+
+        register(context, EBONY_KEY, Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
+                BlockStateProvider.simple(BlockRegister.ROCK_BLOCK.get()),
+                new StraightTrunkPlacer(5,6,3),
+                BlockStateProvider.simple(BlockRegister.ROCK_BLOCK.get()),
+                new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 4),
+                new TwoLayersFeatureSize(1,0,2)).build());
+
+        register(context, OVERWORLD_BLACK_OPAL_ORE_KEY, Feature.ORE, new OreConfiguration(overWorldBlackOpalOres,9));
+        register(context, END_BLACK_OPAL_ORE_KEY, Feature.ORE, new OreConfiguration(endStoneReplaceAbles, BlockRegister.ROCK_BLOCK.get().defaultBlockState(), 9));
+        register(context, NETHER_BLACK_OPEL_ORE_KEY, Feature.ORE, new OreConfiguration(netherRackReplaceAbles, BlockRegister.ROCK_BLOCK.get().defaultBlockState(), 9));
+
+    }
+
+    //将这种矿石生成类型进行注册
+    public static ResourceKey<ConfiguredFeature<?,?>> registerKey(String name){
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE, new ResourceLocation(Reference.MOD_ID, name));
+    }
+
+    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstapContext<ConfiguredFeature<?, ?>> context,
+                                                                                                      ResourceKey<ConfiguredFeature<? ,?>> key,
+                                                                                                      F feature,
+                                                                                                      FC configuration) {
+        context.register(key, new ConfiguredFeature<>(feature, configuration));
+    }
 }
--- src/main/java/com/leisure/miniuav/world/feature/ModPlacedFeatures.java
+++ src/main/java/com/leisure/miniuav/world/feature/ModPlacedFeatures.java
@@ -1,30 +1,74 @@
 package com.leisure.miniuav.world.feature;
 
+import com.leisure.miniuav.block.BlockRegister;
 import com.leisure.miniuav.utils.Reference;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.VegetationPatchConfiguration;
+import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 import net.minecraft.world.level.levelgen.placement.*;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.lang.module.Configuration;
 import java.util.List;
 
 import static com.leisure.miniuav.world.feature.ModOrePlacement.commonOrePlacement;
-//https://forums.minecraftforge.net/topic/110798-1182-crash-when-adding-custom-tree-to-biome/
 public class ModPlacedFeatures {
-//    public static final DeferredRegister<PlacedFeature> PLACED_FEATURES =
-//            DeferredRegister.create(Registry.PLACED_FEATURE_REGISTRY, Reference.MOD_ID);
-//
-//    //矿石摆放
-//    public static final RegistryObject<PlacedFeature> FANTOM_ORE_PLACED = PLACED_FEATURES.register("fantom_ore_placed",
-//            () -> new PlacedFeature(ModConfiguredFeatures.FANTOM_ORES.getHolder().get(),
-//                    commonOrePlacement(7, //每个区块生成多少矿石
-//                            HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(-60), VerticalAnchor.aboveBottom(60))))); //-60，60分别指矿石生成高度范围介于[-60,60]
-//
-//
-//
-//    public static void register(IEventBus eventBus) {
-//        PLACED_FEATURES.register(eventBus);
-//    }
+    public static final ResourceKey<PlacedFeature> EBONY_PLACES_KEY = createKey("ebony_placed");
+    public static final ResourceKey<PlacedFeature> BLACK_OPAL_PLACES_KEY = createKey("black_opal_ore");
+    public static final ResourceKey<PlacedFeature> END_BLACK_OPAL_PLACES_KEY = createKey("end_black_opal_ore");
+    public static final ResourceKey<PlacedFeature> NETHER_BLACK_OPEL_PLACES_KEY = createKey("nether_black_opel_ore");
+    public static void bootstrap(BootstapContext<PlacedFeature> context){
+        HolderGetter<ConfiguredFeature<?,?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
+
+        register(context, EBONY_PLACES_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.EBONY_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(3,0.1f, 2), BlockRegister.ROCK_BLOCK.get()));
+
+        register(context, BLACK_OPAL_PLACES_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_BLACK_OPAL_ORE_KEY),
+                ModOrePlacement.commonOrePlacement(16, HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-64), VerticalAnchor.absolute(80))));
+        register(context, NETHER_BLACK_OPEL_PLACES_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.NETHER_BLACK_OPEL_ORE_KEY),
+                ModOrePlacement.commonOrePlacement(9, HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-64), VerticalAnchor.absolute(80))));
+        register(context, END_BLACK_OPAL_PLACES_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.END_BLACK_OPAL_ORE_KEY),
+                ModOrePlacement.commonOrePlacement(9, HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-64), VerticalAnchor.absolute(80))));
+
+
+    }
+
+    //矿石摆放
+    private static ResourceKey<PlacedFeature> createKey( String name){
+        return ResourceKey.create(Registries.PLACED_FEATURE, new ResourceLocation(Reference.MOD_ID, name));
+    }
+
+    private static void register(BootstapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key, Holder<ConfiguredFeature<?,?>> configuration,
+                                 List<PlacementModifier> modifiers) {
+        context.register(key, new PlacedFeature(configuration, List.copyOf(modifiers)));
+    }
+
+    private static void register(BootstapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key, Holder<ConfiguredFeature<?,?>> configuration,
+                                 PlacementModifier... modifiers) {
+        register(context, key, configuration, List.of(modifiers));
+    }
 }
