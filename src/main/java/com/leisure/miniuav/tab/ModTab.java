906d8a14d43b911166fad3a7d86cb98e2187b855
leisure <491653240@qq.com>
2023-06-01 09:28:17 +0800

    补充item的注册和材质

--- src/main/java/com/leisure/miniuav/tab/ModTab.java
+++ src/main/java/com/leisure/miniuav/tab/ModTab.java
@@ -4,16 +4,14 @@ import com.leisure.miniuav.utils.Reference;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.event.CreativeModeTabEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-//@Mod.EventBusSubscriber(modid = Reference.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModTab {
 
-    public static CreativeModeTab TEST_TAB;
+    public static CreativeModeTab UAV_TAB;
 
     @SubscribeEvent
     public static void registerTabs(CreativeModeTabEvent.Register event) {
-        TEST_TAB = event.registerCreativeModeTab(new ResourceLocation(Reference.MOD_ID, "uav_tab"), builder -> new TestTab().output(builder));
+        UAV_TAB = event.registerCreativeModeTab(new ResourceLocation(Reference.MOD_ID, "uav_tab"), builder -> new UavTab().output(builder));
     }
 }
--- src/main/java/com/leisure/miniuav/tab/UavTab.java
+++ src/main/java/com/leisure/miniuav/tab/UavTab.java
@@ -6,13 +6,27 @@ import net.minecraft.world.item.ItemStack;
 import com.leisure.miniuav.init.ItemInit;
 import com.leisure.miniuav.init.BlockInit;
 
-public class TestTab {
+public class UavTab {
 
     public CreativeModeTab output(CreativeModeTab.Builder builder) {
         return builder.title(Component.translatable("itemGroup.uav_tab"))
                 .icon(() -> new ItemStack(ItemInit.LEI.get()))
                 .displayItems((featureFlags, output, hasOp) -> {
                     output.accept(ItemInit.LEI.get());
+                    output.accept(ItemInit.FRUIT.get());
+                    output.accept(ItemInit.EFFECTIVE_FRUIT.get());
+                    output.accept(ItemInit.FUEL.get());
+                    output.accept(ItemInit.TELEPORT_STAFF.get());
+                    output.accept(ItemInit.PINK_SWORD.get());
+                    output.accept(ItemInit.PINK_PICKAXE.get());
+                    output.accept(ItemInit.PINK_AXE.get());
+                    output.accept(ItemInit.PINK_SHOVEL.get());
+                    output.accept(ItemInit.PINK_HOE.get());
+                    output.accept(ItemInit.PINK_HELMET.get());
+                    output.accept(ItemInit.PINK_CHESTPLATE.get());
+                    output.accept(ItemInit.PINK_LEGGINGS.get());
+                    output.accept(ItemInit.PINK_BOOTS.get());
+                    output.accept(ItemInit.EXPLOSIVE_ARROW.get());
                     output.accept(BlockInit.ROCK_BLOCK.get());
                     output.accept(BlockInit.EXAMPLE_METAL.get());
                     output.accept(BlockInit.FANTOM_ORE.get());
